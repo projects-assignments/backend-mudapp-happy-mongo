69ba9ad191fe811944bf4f92eb17c7c3c9848d76
RuthDanielaAguirre <argdiee@icloud.com>
2024-02-04 23:26:53 +0100

    creando el swagger

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix('api/v1');
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('Mudapp')
-  //   .setDescription(
-  //     'Our company is dedicated for moving services around Barcelona',
-  //   )
-  //   .setVersion('1.0')
-  //   .addTag('moving')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
-  await app.listen(5000);
+  const config = new DocumentBuilder()
+  .setTitle('MUDAPP HAPPYMONGO')
+  .setDescription('The Best Moving App')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .addTag('Lets move to Barcelona')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('MUDAPPHM', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
-
-// esto va entre las lineas: "const app" y "await app" //
